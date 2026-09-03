d54c1c5b6b728264b7406d5fef434e396bac6ce9
haritha.sammeta <harithasammeta123@gmail.com>
2026-09-03 23:25:03 +0530

    fix

--- Patterns/pattern5.java
+++ Patterns/pattern5.java
@@ -6,9 +6,11 @@
 * * * 
 * * * * 
 * * * * * 
+* * * * 
 * * * 
 * * 
 * 
+
 
  */
 
@@ -26,7 +28,7 @@ class Patterns5 {
     }
     static void pattern5(int n){
       for(int row =0; row< 2 * n; row++){
-         int totalRows= row > n ? 2 * n - row-1 : row; 
+         int totalRows= row > n ? 2 * n - row: row; 
         for(int col = 0; col<totalRows; col++){
           System.out.print("* ");
         }
