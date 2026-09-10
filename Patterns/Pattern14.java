eedd3cdfee71c0c64fa7b4ad3dfffeb5543a5509
haritha.sammeta <harithasammeta123@gmail.com>
2026-09-10 10:38:11 +0530

    Updated the logics

--- Patterns/Pattern14.java
+++ Patterns/Pattern14.java
@@ -5,14 +5,15 @@ public class Pattern14{
     static void pattern1(int n){
         for(int row=1; row <=n; row++){
             //space
-            for(int space=1; space <=n-row-1; space++){
+            for(int space=1; space <=n-row;space++){
                 System.out.print(" ");
             }
             for(int col=1; col<=2*row-1; col++){
+
                 System.out.print(row);
             }
             //space
-            for(int space=1; space <=n-row-1; space++){
+            for(int space=1; space <=n-row; space++){
                 System.out.print(" ");
             }
             System.out.println();
--- Patterns/Pattern15.java
+++ Patterns/Pattern15.java
@@ -1,4 +1,4 @@
-public class Pattern10 {
+public class Pattern15{
     public static void main(String[] args) {
         pattern1(5);
     }
@@ -9,10 +9,17 @@ public class Pattern10 {
                 System.out.print(" ");
             }
             for(int col=1; col<=2*row-1; col++){
-                System.out.print("*");
+                int value=n-col+1;
+                if(value>0){
+                    System.out.print(value);
+                }
+                else{
+                    System.out.print(col-n+1);
+                }
+                // System.out.print(value);
             }
             //space
-            for(int space=1; space <=n-row;space++){
+            for(int space=1; space <=n-row; space++){
                 System.out.print(" ");
             }
             System.out.println();
